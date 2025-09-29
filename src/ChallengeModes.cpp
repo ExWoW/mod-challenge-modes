7fa2757ff92579728ef4457c4e374b6e2b8d0b6d
IWeidl <isaiah.weidl@gmail.com>
2025-09-29 22:20:41 +1000

    Update Hardcore mode item rewards and adjust visibility conditions for DKs.

--- src/ChallengeModes.cpp
+++ src/ChallengeModes.cpp
@@ -801,7 +801,7 @@ public:
 
         bool CanBeSeen(Player const* player) override
         {
-            if ((player->GetLevel() > 1 && player->getClass() != CLASS_DEATH_KNIGHT) || (player->GetLevel() > 55))
+            if (player->GetLevel() > 1 || player->getClass() == CLASS_DEATH_KNIGHT)
             {
                 return false;
             }
